3fede3519818f3838058f15a084a50b44768d8dc
Vaibhavi-Sita <52885102+Vaibhavi-Sita@users.noreply.github.com>
2023-02-12 22:49:25 +0530

    firebase init

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7G3gRNT5iBGgDU2qbBMkufBoxXZb0jwY",
+  authDomain: "my-unsplash-1ce9e.firebaseapp.com",
+  projectId: "my-unsplash-1ce9e",
+  storageBucket: "my-unsplash-1ce9e.appspot.com",
+  messagingSenderId: "471371063544",
+  appId: "1:471371063544:web:a7bd18f9c7eec71eb31b0d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
